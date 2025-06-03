f8e99393945d5e4a9b1044136d1423eb43705542
Leena Baniodeh <lena.oudeh@gmail.com>
2025-06-03 13:49:57 +0300

    add task export to routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,4 @@ use App\Http\Controllers\TaskController;
 
 Route::get('/', [TaskController::class, 'index']);
 Route::resource('tasks', TaskController::class)->except('show');
+Route::get('tasks-export', [TaskController::class, 'export'])->name('tasks.export');
